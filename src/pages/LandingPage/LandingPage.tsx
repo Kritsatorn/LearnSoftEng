84cdc9d9ad4349e16c5c63d881c7fe35fcfa47d6
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2020-11-07 12:45:17 +0700

    d

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,40 +1,33 @@
-import * as React from 'react';
-import { LandingPageTypes } from './LandingPageTypes';
-import { SeasonDisplay } from '../../components/SeasonDisplay/SeasonDisplay';
-import { BarCard } from '../../components/BarCard/BarCard';
-export class LandingPage
-  extends React.Component<object, LandingPageTypes> {
+import * as React from "react";
+import { LandingPageTypes } from "./LandingPageTypes";
+import { SeasonDisplay } from "../../components/SeasonDisplay/SeasonDisplay";
+import { BarCard } from "../../components/BarCard/BarCard";
+export class LandingPage extends React.Component<object, LandingPageTypes> {
   constructor(props: object) {
     super(props);
 
     this.state = {
-      text: 'Hello',
+      text: "Hello",
       lat: 0,
-      month: 0
-    }
-
+      month: 0,
+    };
   }
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      position => (this.setState({ lat: position.coords.latitude })),
-      err => console.log(err.message)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => console.log(err.message)
     );
-    this.setState({ month: new Date().getMonth() })
+    this.setState({ month: new Date().getMonth() });
   }
 
   render() {
-
     return (
       <div>
-        <BarCard
-          cardName={this.state.text}
-        />
+        <BarCard cardName={this.state.text} />
         <br />
-        <SeasonDisplay
-          lat={this.state.lat}
-          month={this.state.month}
-        />
+        <SeasonDisplay lat={this.state.lat} month={this.state.month} />
+        <div> HII world</div>
       </div>
     );
   }
